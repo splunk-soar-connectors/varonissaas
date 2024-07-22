b3adf0cd86a5d5aa7fe526f515e225fc31c49760
Volodymyr Korenkov <vkorenkov@varonis.com>
2024-07-22 21:16:19 +0300

    import sorting

--- varonissaas_connector.py
+++ varonissaas_connector.py
@@ -41,7 +41,7 @@ from urllib3 import Retry
 import varonissaas_tools as tools
 from varonissaas_consts import *
 from varonissaas_search import (ALERT_SEVERITIES, ALERT_STATUSES, CLOSE_REASONS, AlertItem, EventItem, SearchAlertObjectMapper,
-                               SearchEventObjectMapper, SearchRequest, create_alert_request, create_alerted_events_request, get_query_range)
+                                SearchEventObjectMapper, SearchRequest, create_alert_request, create_alerted_events_request, get_query_range)
 
 REQUEST_RETRIES = 30
 HTTP_STATUS_WHITE_LIST = [ 304, 206 ]
